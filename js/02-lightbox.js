d46431e815a1649649c592393300786a59bca39d
IYuliia <catnew2012@gmail.com>
2023-10-20 12:26:42 +0200

    task two - added markup and modal window

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
+gallery.addEventListener("click", handleClick);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function handleClick(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("gallery__image")) {
+    const images = gallery.querySelectorAll(".gallery__image");
+    const index = Array.from(images).indexOf(e.target);
+
+    lightbox.open(index);
+  }
+}
+
+function createMarkup(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join("");
+}
