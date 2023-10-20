c9e03766329da909aabbf6ed68073bdb46b47082
IYuliia <catnew2012@gmail.com>
2023-10-20 13:04:54 +0200

    fixed handleClick function and added Escape Key exit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,32 +4,30 @@ import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
+let instance;
 
 gallery.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
 console.log(gallery);
 
 gallery.addEventListener("click", handleClick);
+gallery.addEventListener("keydown", handleKey);
 
 function handleClick(e) {
   e.preventDefault();
+  console.log(e.target);
 
-  if (e.target.classList.contains("gallery__link")) {
-    const galleryItem = e.target.closest(".gallery__item");
-    if (galleryItem) {
-      const original = e.target.getAttribute("data-source");
-      const description = e.target.getAttribute("alt");
-      showModal({ original, description });
-    }
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
   }
+  const original = e.target.getAttribute("data-source");
+  const description = e.target.getAttribute("alt");
+  showModal({ original, description });
 }
 
 function showModal({ original, description }) {
-  const instance = basicLightbox.create(`
-          <img
-            data-source="${original}"
-            alt="${description}"
-          />
-`);
+  instance = basicLightbox.create(`
+    <img src="${original}" alt="${description}" />
+  `);
   instance.show();
 }
 
@@ -48,4 +46,11 @@ function createMarkup(arr) {
       </li>`;
     })
     .join("");
+}
+
+function handleKey(e) {
+  console.log(e.code);
+  if (e.code === "Escape") {
+    instance.close();
+  }
 }
